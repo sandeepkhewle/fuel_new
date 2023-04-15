90c22f2ba0f0f7b58d395407d6916a2cd2b29a3d
Swapnil Chaple <chapleswapnil@gmail.com>
2023-04-15 17:24:53 +0530

    added email id in the check device api responce

--- server/services/user.service.js
+++ server/services/user.service.js
@@ -76,7 +76,7 @@ let registerNewUser = async ({ appId, fullName, phoneNo, emailId, deviceId, pass
 
 let checkDevice = async ({ appId, deviceId, token }) => {
     try {
-        let uData = await userModel.findOneAndUpdate({ appId: appId, deviceId: deviceId }, { token: token, lastActive: new Date() }, { new: true }).select({ phoneNo: 1, userId: 1, appId: 1, phoneNo: 1, emailId: 1, fullName: 1, companyName: 1, cdCounter: 1, cdStatus: 1, cpCounter: 1, declineStatus: 1, deviceId: 1, token: 1, isOtpVerified: 1, referralCode: 1 });
+        let uData = await userModel.findOneAndUpdate({ appId: appId, deviceId: deviceId }, { token: token, lastActive: new Date() }, { new: true }).select({ phoneNo: 1, userId: 1, appId: 1, phoneNo: 1, emailId: 1, fullName: 1, companyName: 1, cdCounter: 1, cdStatus: 1, cpCounter: 1, declineStatus: 1, deviceId: 1, token: 1, isOtpVerified: 1, referralCode: 1, emailId: 1 });
         if (uData) {
             return uData;
         } else {
