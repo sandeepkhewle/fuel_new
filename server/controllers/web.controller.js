bb4feeb671fd93bc0b5af67ebc1cc42ed78fc85b
Swapnil Chaple <chapleswapnil@gmail.com>
2023-05-17 12:37:33 +0530

    ver change

--- server/controllers/web.controller.js
+++ server/controllers/web.controller.js
@@ -233,7 +233,7 @@ router.post('/checkDevice', (req, res) => {
             "message": "Device found",
             "payload": payload,
             "jwt": tokenData,
-            "version": { android: { versionNo: "6.0.0", force: false }, ios: { versionNo: "1.0", force: false } }
+            "version": { android: { versionNo: "4.0.0", force: false }, ios: { versionNo: "1.0", force: false } }
         });
     }).catch(err => {
         console.log('err', err);
