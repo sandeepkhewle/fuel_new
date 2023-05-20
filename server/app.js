f1c95a12b2487a218b9d158c350574b97dae60fb
Swapnil Chaple <chapleswapnil@gmail.com>
2023-05-20 16:35:43 +0530

    privacy policy added

--- server/app.js
+++ server/app.js
@@ -38,6 +38,9 @@ app.use('/.well-known/acme-challenge/KvmIqbTsTNypSAzuglWFzQXrs0EbT_wJgsfa5cxhnA8
     }
 });
 
+app.get('/privacyPolicy', function (req, res) { res.render(__dirname + '/views/privacyPolicy.html') });
+
+
 
 // run https server in the production
 if (process.env.NODE_ENV === "production") {
